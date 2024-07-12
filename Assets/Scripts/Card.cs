7ba1f28427393b73f1dbd1081e51e2876bf50531
marsborox <162184787+marsborox@users.noreply.github.com>
2024-07-12 22:33:43 +0200

    have card prefab some scripts

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] string type;
+    [SerializeField] int value;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
