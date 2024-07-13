8f6633a45e65de15a4220151c807d5849f7244b9
marsborox <mierkamaros@gmail.com>
2024-07-13 21:13:27 +0200

    card spawn points

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    [SerializeField] string type;
+    // 0 monster 1 monster 2 potion 3 weapon
+    [SerializeField] byte type;
     [SerializeField] int value;
     // Start is called before the first frame update
     void Start()
