7bd16d942cb71132f131e1fe65746948dde63f62
marsborox <162184787+marsborox@users.noreply.github.com>
2024-07-15 22:02:38 +0200

    card pull method
    card pull method
    progress in method that pulls cards from deck

--- Assets/Scripts/DungeonDeck.cs
+++ Assets/Scripts/DungeonDeck.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class DungeonDeck : MonoBehaviour
 {
-    
+    [SerializeField] GameObject monster0;
+    [SerializeField] GameObject monster1;
+    [SerializeField] GameObject potion;
+    [SerializeField] GameObject weapon;
     [SerializeField] GameObject spawnPoints;
 
     List<int> monsterList0 = new List<int>();
     List<int> monsterList1 = new List<int>();
     List<int> potionList = new List<int>();
     List<int> weaponList = new List<int>();
+
+    Dictionary<int, List<int>> cardNumberType= new Dictionary<int, List<int>>();
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +24,15 @@ public class DungeonDeck : MonoBehaviour
         CreateCardsOfType(15,monsterList1);
         CreateCardsOfType(10,potionList);
         CreateCardsOfType(10,weaponList);
+
+        cardNumberType.Add(0, monsterList0);
+        cardNumberType.Add(1, monsterList1);
+        cardNumberType.Add(2, potionList);
+        cardNumberType.Add(3, weaponList);
+    }
+    void Update()
+    {
+
     }
     void CreateCardsOfType(int lenght,List <int> list)
     {
@@ -28,6 +43,59 @@ public class DungeonDeck : MonoBehaviour
                 list.Add(insertedValue);
             }
         }
+    }
+
+    void PickEmptySpawnPoint()
+    { 
+    
+    }
+
+    void PulLCardRetardedIQ0Method()
+    {
+        int typeKey = Random.Range(0, 4);
+        if (typeKey == 0)
+        {
+            ParticularCard(monsterList0);
+            Instantiate(monster0);
+            //setValue
+        }
+        else if (typeKey == 1)
+        {
+            ParticularCard(monsterList1);
+            Instantiate(monster1);
+            //setValue
+        }
+        else if (typeKey == 2)
+        {
+            ParticularCard(potionList);
+            Instantiate(potion);
+            //setValue
+        }
+        else if (typeKey == 3)
+        {
+            ParticularCard(weaponList);
+            Instantiate(weapon);
+            //setValue
+        }
+    }
+    void PullCard()
+    {
+        
+        int typeKey =  Random.Range(0, 4);
+        if (cardNumberType.TryGetValue(typeKey, out var returnedValue))
+        {
+            ParticularCard(returnedValue); 
+        }
+    }
+    void ParticularCard(List <int> list)
+    {
+        int randomCard = Random.Range(0, list.Count);
+
+    }
+
+    void SpawnCard()
+    {
+        
     }
 
     public void PrintAllLists()
@@ -44,8 +112,5 @@ public class DungeonDeck : MonoBehaviour
         Debug.Log(string.Format("here is output for list " + nameOfList + ": " + string.Join(" ,",list)));
     }
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
