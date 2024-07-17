942039f22adec02b894e2c31835b0f2e0e21565a
marsborox <162184787+marsborox@users.noreply.github.com>
2024-07-17 21:56:15 +0200

    cards working with sprites
    as card is generated, values on card are properly assigned and sprite picked
    readjusted sprites

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using TMPro;
 
 public class Card : MonoBehaviour
 {
@@ -10,7 +11,17 @@ public class Card : MonoBehaviour
     [SerializeField] int cardValue;
     [SerializeField] string description;//remove later
 
+    [SerializeField] SpriteRenderer cardImage;
     
+    [SerializeField] Sprite monster0;
+    [SerializeField] Sprite monster1;
+    [SerializeField] Sprite potion;
+    [SerializeField] Sprite weapon;
+
+    [SerializeField] TextMeshPro valueText;
+    [SerializeField] TextMeshPro valueText2;
+    [SerializeField] TextMeshPro typeText;
+
     bool toldinfo=false;
     DungeonDeck dungeonDeck;
     // Start is called before the first frame update
@@ -24,6 +35,7 @@ public class Card : MonoBehaviour
         Debug.Log("PullingCardInfo");
         dungeonDeck.PulLCardMethod();
         SetProperties();
+        SetCardSpriteColor();
     }
 
     // Update is called once per frame
@@ -48,9 +60,42 @@ public class Card : MonoBehaviour
     {
         
     }
+    void SetCardSpriteColor()
+    {   
+        cardImage.color = Color.white;
+    }
     public void SetProperties()
     {
         cardType = dungeonDeck.tempType;
         cardValue = dungeonDeck.tempValue;
+
+        valueText.text = cardValue.ToString();
+        valueText2.text = cardValue.ToString();
+        SetCardType(cardType);
+    }
+    void SetCardType(int typeValue)
+    {
+        switch (typeValue)
+        {
+            case  0:
+                typeText.text= "Monster0";
+                cardImage.sprite = monster0;
+                break;
+            case 1:
+                typeText.text= "Monster1";
+                cardImage.sprite = monster1;
+                break;
+            case 2:
+                typeText.text= "Potion";
+                cardImage.sprite = potion;
+                break;
+            case 3:
+                typeText.text= "Weapon";
+                cardImage.sprite = weapon;
+                break;
+            default: ;
+                break;
+        }
+            
     }
 }
